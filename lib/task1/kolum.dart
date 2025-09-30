8d2abdff75134c2f9d3bd92a14ceb69be1021918
ITZARYAN18 <aryan0901k@gmail.com>
2025-09-30 18:31:51 +0530

    Initial commit

---
+++ lib/task1/kolum.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ColumnTrial extends StatelessWidget {
+  const ColumnTrial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Columns"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 20,),
+            Padding(padding: EdgeInsets.all(10)),
+          Center
+            (
+
+            child: Text("Welcome To Flutter Layouts",style: TextStyle(
+            fontSize: 30
+          ),),
+
+          ),
+          SizedBox(height: 10,),
+          Container(
+            child: Text("Welcome",style: TextStyle(
+              fontSize: 20
+            ),),
+          ),
+          SizedBox(height: 40,),
+          Container(
+            child: ElevatedButton(onPressed: (){}, child: Text("Get Started")),
+          )
+
+        ],
+      ),
+    );
+  }
+}

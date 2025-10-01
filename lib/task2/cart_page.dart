88c6290376a8c87ba156719ffee7119566421b18
ITZARYAN18 <aryan0901k@gmail.com>
2025-10-01 20:43:28 +0530

    cart page adding

--- lib/task2/cart_page.dart
+++ lib/task2/cart_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/task2/model.dart';
+import 'package:provider/provider.dart';
+
+import 'cart_provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product Details"),
+      ),
+      body: Container(
+        child: ElevatedButton(
+          onPressed: () {
+            Provider.of<CartProvider>(context, listen: false).addToCart(Product);
+          },
+          child: Text("Add to Cart"),
+        )
+        ,
+      )
+    );
   }
 }

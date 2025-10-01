64ee414aadc3c298f3e975ef2ca47f18e67f83a4
ITZARYAN18 <aryan0901k@gmail.com>
2025-10-01 20:39:49 +0530

    added cart provider

--- lib/task2/cart_provider.dart
+++ lib/task2/cart_provider.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'model.dart';
+
 class CartProvider extends ChangeNotifier{
+  final List<Product> _cart = [];
+
+  void addToCart(Product product) {
+    _cart.add(product);
+    notifyListeners();
+  }
 
 }
